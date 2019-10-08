b2012b5fc499b564b198985a16f3bfdc38ec3441
Francisco-Cubo_Cimat <fco.valente.castro@gmail.com>
2019-10-08 13:17:54 -0500

    Update pseudorandom.py

--- Tarea_5/pseudo_random.py
+++ Tarea_5/pseudo_random.py
@@ -5,6 +5,9 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Constant 
+maxInt = 2147483647  # 2^31 - 1
+
 # Parser arguments
 parser = argparse.ArgumentParser(
     description='Random number generator')
@@ -22,7 +25,7 @@ def linear_congurent_generator(sample_size, seed=1):
     x = np.ones(5 + sample_size) * seed
 
     # Divisor for modulo operation
-    div = 2147483647  # 2^31 - 1
+    div = maxInt  # 2^31 - 1
 
     # Simulate for #sample_size of iterations
     for idx in range(5, sample_size + 5):
@@ -50,67 +53,57 @@ def line_coef(x_1, x_2):
 
 def line_intersection(coef_1, coef_2):
     # Unpack coeficients
-    b1, a1 = coef_1
-    b2, a2 = coef_2
+    a1, b1 = coef_1
+    a2, b2 = coef_2
 
     # Caculate intersection
-    x = (a1 - a2) / (b2 - b1)
-    y = b1 * x + a1
+    x = (b2 - b1) / (a1 - a2)
+    y = a1 * x + b1
 
     return (x, y)
 
 
-def lower_adaptive_envelope(x, points):
+def envelope(x, points):
     # Number of points
     n = len(points)
 
     # Find position of x in the array
     x_position = 0
-    while x > points[x_position] and x_position < n:
+    while x_position < n and x > points[x_position][0]:
         x_position += 1
 
     # Evaluate point
     if x_position == 0 or x_position == n:
         value = float('-inf')
     else:
-        a, b = line_coef(x,
-                         points[x_position - 1],
-                         points[x_position])
+        a, b = line_coef(points[x_position - 1], points[x_position])
         value = a * x + b
 
     return np.exp(value)
 
-
-def uppper_adaptive_envelope(x, points):
-    # Number of points
-    n = len(points)
-
-    # Find position of x in the array
-    x_position = 0
-    while x > points[x_position] and x_position < n:
-        x_position += 1
-
-    # Evaluate point
-    if x_position == 0 or x_position == n:
-        value = float('-inf')
-    else:
-        a, b = line_coef(x,
-                         points[x_position - 1],
-                         points[x_position])
-        value = a * x + b
-
-    return np.exp(value)
 
 
 def points_of_upper_adaptive_envelope(points):
     # Points on upper envelope
     envelope = []
 
+    # Slope of first and last piece 
+    slope = .1
+    y_value = -10
+
+    # Calculate initial piece 
+    x1, y1 = points[0]
+    a1, b1 = (slope, y1 - slope * x1)
+
+    # Calculate last piece
+    x2, y2 = points[-1]
+    a2, b2 = (-slope, y2 + slope * x2)
+
     # Add points in extremes
-    points = [(float('-inf'), float('-inf'))] + points
+    points = [((y_value - b1)/a1, y_value)] + points + [((y_value - b2)/a2, y_value)]
 
     for idx, p in enumerate(points):
-        if idx == 1:
+        if idx <= 1 or idx == len(points) - 1:
             envelope.append(p)
         elif idx > 1 and idx < len(points) - 1:
             coef_1 = line_coef(points[idx - 2], points[idx - 1])
@@ -124,55 +117,61 @@ def points_of_upper_adaptive_envelope(points):
 
 def sample_from_upper_adaptive_envelope(points):
     # Common length
-    n = len(points) + 2
+    n = len(points)
 
     # Construct piecewise envelope
     C = [0] * n  # (a,b)
     W = [0] * n  # scalar
 
     # Integrate picewise density
-    # First integral
-    x, y = points[0]
-    a, b = C[0] = (.001, y - (.001) * x)
-    W[0] = np.exp(a * x + b) / a
-
-    # Intermediate integral
     for idx, p in enumerate(points):
         if idx > 0:
+            # Line equation from piece
             x1, y1 = points[idx - 1]
             x2, y2 = points[idx]
+            a, b = C[idx  - 1] = line_coef(points[idx - 1], points[idx])
 
-            a, b = C[idx] = line_coef(points[idx - 1],
-                                      points[idx])
-            W[idx] = np.exp(b) * (np.exp(a * x2) - np.exp(a * x1)) / a
+            # Density weight of piece
+            W[idx - 1] = np.exp(b) * (np.exp(a * x2) - np.exp(a * x1)) / a
 
-    # Last integral
-    x, y = points[-1]
-    a, b = C[-1] = (-.001, y - (-.001) * x)
-    W[-1] = -np.exp(a * x + b) / a
-
-    # Complete integral
+    # Sum all weights and transform W to density
     weight = np.sum(W)
     W = W / weight
-
-    print(W)
 
     # Pick random piece
     rnd = np.random.rand()
     x_position = sigma = 0
-    while rnd > sigma + W[x_position] and x_position - 1 < n:
-        x_position += 1
+    while x_position - 1 < n and rnd > sigma + W[x_position]:
         sigma += W[x_position]
+        x_position += 1
 
     # Use density on piece
+    rnd = np.random.rand()
     x1, y1 = points[x_position]
     x2, y2 = points[x_position + 1]
     a, b = C[x_position]
-    sample = np.log(np.exp(a * x) +
+    sample = np.log(np.exp(a * x1) +
                     rnd * (np.exp(a * x2) - np.exp(a * x1))) / a
 
     return sample
 
+
+def plot_envelopes(density, low_env, up_env, sample, y_sample):
+    def lower(x):
+        return envelope(x, low_env)
+    def upper(x):
+        return envelope(x, up_env)
+
+    x_range = np.linspace(0,10,100)
+    y_density = [density(x) for x in x_range]
+    y_lower = [lower(x) for x in x_range]
+    y_upper = [upper(x) for x in x_range]
+
+    plt.plot(x_range, y_density)
+    plt.plot(x_range, y_upper)
+    plt.plot(x_range, y_lower)
+    plt.scatter(sample, y_sample)
+    plt.show()
 
 def adaptive_rejection_sampling(density, points, sample_size, seed=0):
     # Points for the envelope
@@ -180,33 +179,51 @@ def adaptive_rejection_sampling(density, points, sample_size, seed=0):
 
     # Sample array
     sample = []
+    y_sample = []
 
-    for idx in range(sample_size):
+    plot_envelopes(density, S,
+                   points_of_upper_adaptive_envelope(S),
+                   sample,y_sample)
+
+
+    # Accept #sample_size samples
+    while( len(sample) < sample_size ):
+
+        low_env = S
         def lower(x):
-            return lower_adaptive_envelope(x, S)
+            return envelope(x, low_env)
 
         up_env = points_of_upper_adaptive_envelope(S)
-
         def upper(x):
-            return uppper_adaptive_envelope(x, up_env)
+            return envelope(x, up_env)
 
         # Sample from g_n distribution
-        g_sample = sample_from_upper_adaptive_envelope(S)
+        g_sample = sample_from_upper_adaptive_envelope(up_env)
 
         rnd = np.random.rand()
-        if rnd <= lower(g_sample) / upper(g_sample):
+        if rnd <= lower(g_sample) / upper(g_sample): # Accept
+            # Accept sample
             sample.append(g_sample)
-        elif rnd <= density(g_sample) / upper(g_sample):
+            y_sample.append(rnd * upper(g_sample))
+        elif rnd <= density(g_sample) / upper(g_sample): # Accept and refine
+            # Accept sample
             sample.append(g_sample)
-            S.append(g_sample)
-            S = sorted(S)
+            y_sample.append(rnd * upper(g_sample))
+            
+            # Refine the envelope
+            S.append((g_sample, np.log(density(g_sample))))
+            S.sort()
+
+    plot_envelopes(density, S, 
+                   points_of_upper_adaptive_envelope(S),
+                   sample, y_sample)
 
     return sample
 
 
 def uniform_sampling(sample_size, seed=1, a=0, b=1):
     # Maximum number
-    div = 2147483647  # 2^31 - 1
+    div = maxInt  # 2^31 - 1
 
     # Obtain uniform sample in the range (0,1)
     unif = linear_congurent_generator(sample_size, seed) / div
@@ -234,24 +251,25 @@ def main():
         return d
 
     # Adaptive rejection sampling
-    S = [.1, .5, 1, 1.5, 10]
-    S = [(s, gamma_density(s)) for s in S]
+    S = np.linspace(0.1,5,10)
+    S = [(s, np.log(gamma_density(s))) for s in S]
     adap = adaptive_rejection_sampling(gamma_density,
                                        S,
                                        args.sample_size,
                                        args.seed)
-    print(adap)
 
-    num_bins = 100
+    num_bins = 80
     # the histogram of the data
     n, bins, patches = plt.hist(adap,
                                 num_bins,
                                 density=1,
                                 facecolor='blue',
                                 alpha=0.5)
-    plt.show()
+    x_range = np.linspace(0,10,100)
+    y_density = [gamma_density(x) for x in x_range]
 
-    return
+    plt.plot(x_range, y_density)
+    plt.show()
 
     unif_01 = uniform_sampling(args.sample_size, args.seed, a=0, b=1)
     unif_23 = uniform_sampling(args.sample_size, args.seed, a=2, b=3)
@@ -286,28 +304,6 @@ def main():
     n, bins, patches = plt.hist(exp_3, num_bins, density=1, alpha=0.5)
     plt.show()
 
-    # Calculate eigenvalues of symmetric matrix
-    # for exponent in range(1, 6):
-    #     # Create epsilon
-    #     epsilon = 10 ** exponent
-
-    #     # Create matrix
-    #     A = np.array([[8, 1, 0],
-    #                   [1, 4, epsilon],
-    #                   [0, epsilon, 1]])
-
-    #     # Calculate eigenvalues
-    #     (A_k, Q_k) = qr_algorithm(A, iterations=epsilon)
-
-    #     print("epsilon : ", epsilon)
-    #     print("A : ", A, sep="\n")
-
-    #     # Sort eigenvalues
-    #     sortedIdx = np.argsort(np.diag(A_k))
-
-    #     # Print sorted eigenvalues and eigenvectors
-    #     print("eigenvalues : ", np.diag(A_k)[sortedIdx], end="\n")
-    #     print("eigenvectors : ", Q_k[:, sortedIdx], sep="\n", end="\n\n")
 
 
 if __name__ == "__main__":
